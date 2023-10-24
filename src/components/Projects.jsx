80807ebf25f78c8241097e603517bf16544ae500
Martin Fuks <martinfukss@gmail.com>
2023-10-24 19:17:24 -0300

    FIX; links caidos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -25,10 +25,10 @@ return(
     <Flex className="projects" bg={''} h={"100vh"} w={'100%'} align={'center'} justify={'center'} direction={'column'}>
       {language === "spanish" ? (<Heading color={'white'} fontSize={'4vh'} mb={'3vh'}>Proyectos</Heading>) : (<Heading color={'white'} fontSize={'4vh'} mb={'3vh'}>GALLERY OF PROJECTS</Heading>)}
         <CaptionCarousel></CaptionCarousel>
-        <Flex bg={''} h={'20vh'} w={'50%'} justify={'center'} align={'center'} justifyContent={'space-around'}>
+        {/* <Flex bg={''} h={'20vh'} w={'50%'} justify={'center'} align={'center'} justifyContent={'space-around'}>
           <Link to={'https://wondertoys.up.railway.app/'} target="'blank" ><Image _hover={{ transform: "scale(1.1)", transition: "transform 0.3s ease-in-out" }} h={'15vh'} src="/wonderlogo.png"></Image></Link>
           <Link to={'https://stringsandkeys.up.railway.app/'} target="'blank" ><Image _hover={{ transform: "scale(1.1)", transition: "transform 0.3s ease-in-out" }} h={'15vh'} src="/Logo White.png"></Image></Link>
-        </Flex>
+        </Flex> */}
     </Flex>
 )
 }
